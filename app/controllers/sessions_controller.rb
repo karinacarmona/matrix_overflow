365bf11679a344f1bd66e47ac8fb2918b28bf68b
maggie <myustinreyes@gmail.com>
2014-12-12 11:38:01 -0500

    adding sessions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,12 @@
+class SessionsController < ApplicationController
+  def new
+    #should this be doing something?
+  end
+
+  def create
+
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -11,9 +11,9 @@ Rails.application.routes.draw do
   resources :users
 
   #SESSIONS routes
-  # get('/login') => 'sessions#new'
-  # post('/login') => 'sessions#create'
-  # delete('/logout') => 'sessions#destroy'
+  get ('/login') => 'sessions#new'
+  post ('/login') => 'sessions#create'
+  delete ('/logout') => 'sessions#destroy'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 

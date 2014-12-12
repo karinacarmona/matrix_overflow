1b63f38a244e42dd686304a2782428f57f3a3bdd
Liam McCartney <liam.mccartney@gmail.com>
2014-12-11 20:33:23 -0500

    implement strong params

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,11 +10,17 @@ class CommentsController < ApplicationController
   def create
     post = Post.find(params[:post_id])
     p params
-    @comment = post.comments.new(params[:comment])
+    @comment = post.comments.new(comment_params)
     if @comment.save
       redirect_to root_path
     else
       render :new
     end
   end
+
+  private
+
+    def comment_params
+      params.require(:comment).permit(:content)
+    end
 end

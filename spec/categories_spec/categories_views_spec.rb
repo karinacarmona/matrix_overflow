3cc936a4b16f9c8218cbd5c97044214c9d9d606e
maggie <myustinreyes@gmail.com>
2014-12-12 10:04:35 -0500

    organized more tests for categories

---
+++ spec/categories_spec/categories_views_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+RSpec.describe "categories#index", :type => :view do
+  context "on the index page" do
+    describe "the page" do
+      xit "should have a list of all categories available" do
+        expect(true).to eq(true)
+      end
+    end
+  end
+end

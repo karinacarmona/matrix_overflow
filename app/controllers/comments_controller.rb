ba018c5aaabb2915b244e8f1960cdc7a0527f9e3
Liam McCartney <liam.mccartney@gmail.com>
2014-12-11 19:33:03 -0500

    add new method

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,4 +2,8 @@ class CommentsController < ApplicationController
   def index
     @comments = Post.find(params[:post_id]).comments
   end
+
+  def new
+    @comment = Post.find(params[:post_id]).comments.new
+  end
 end

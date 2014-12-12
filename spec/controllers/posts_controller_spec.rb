c6865a0aa9d9070cf5073c4d039e766423b5f3be
Liam McCartney <liam.mccartney@gmail.com>
2014-12-12 11:37:23 -0500

    index test

---
+++ spec/controllers/posts_controller_spec.rb
@@ -0,0 +1,56 @@
+require 'rails_helper'
+
+describe PostsController do
+  let(:test_category) {FactoryGirl.create :category}
+  let(:test_post) {FactoryGirl.create :post}
+
+ describe "#index" do
+    it "assigns the posts to Category.posts" do
+      get :index, :category_id => test_category.id
+      expect(response).to be_success
+    end
+  end
+
+  # describe "#new" do
+  #   it "assigns the comment to Comment.new" do
+  #     get :new, :post_id => test_post.id
+  #     expect(assigns(:comment)).to be_a_new(Comment)
+  #   end
+  # end
+
+  # describe "#create" do
+  #   it "creates the comment if valid params" do
+  #     expect {
+  #       post :create,
+  #       :post_id => test_post.id,
+  #       :comment => { content: 'my content'}
+  #     }.to change { Comment.count }.by(1)
+  #   end
+  # end
+
+  # describe "#edit" do
+  #   it "assigns the comment to the comment" do
+  #     get :edit, post_id: test_post.id, id: test_comment.id
+  #     expect(assigns(:comment)).to eq(test_comment)
+  #   end
+  # end
+
+  # describe "#update" do
+  #   it "updates the comment if valid params" do
+  #     expect {
+  #       put :update, post_id: test_post.id, id: test_comment.id, comment: {
+  #         content: 'my new content',
+  #       }
+  #     }.to change { test_comment.reload.content }.to('my new content')
+  #   end
+  # end
+
+  # describe "#destroy" do
+  #   it "deletes the comment" do
+  #     test_comment
+  #     expect {
+  #       delete :destroy, post_id: test_post.id, id: test_comment.id
+  #     }.to change {Comment.count}.by(-1)
+  #   end
+  # end
+end

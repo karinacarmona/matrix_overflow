b0eb0645c4fc25fb426f22f2f54ff9e34418f8ab
Liam McCartney <liam.mccartney@gmail.com>
2014-12-11 19:02:25 -0500

    test validation failure

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,6 +2,13 @@ require 'rails_helper'
 describe Comment do
   context "validations" do
     it { should validate_presence_of :content }
+    it 'content must be present' do
+      expect {
+        comment = Comment.new
+        comment.save
+        expect(comment.errors.full_messages).to include("Content can't be blank")
+      }.not_to change {Comment.count}
+    end
   end
 
   context "associations" do

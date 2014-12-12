1550155bb1d82c388aad8c94eafe40cd3104f18a
Liam McCartney <liam.mccartney@gmail.com>
2014-12-12 12:10:19 -0500

    test for update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,6 +26,15 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def update
+    @post = Post.find(params[:id])
+    if @post.update_attributes(post_params)
+      redirect_to category_post_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def post_params
